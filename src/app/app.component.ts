9ed6fecde48550105843d1538f041c48b593b8d1
kishorcse <saikishor.patil@gmail.com>
2018-07-14 15:36:20 +0530

    google transloator added on landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+declare let google: any;
+
+declare global {
+  interface Window { google: any; }
+}
+window.google = window.google || {};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    this.addScript();
+  }
+
+
+  addScript () {
+    let script = document.createElement('script');
+    script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    script.onload = () => this.translate()
+    document.body.appendChild(script);
+  }
+
+  translate() {
+    const lang = 'fr';
+    setTimeout(() => {
+      if (!google && window.google) {
+        google = window.google;
+      }
+      const newTrSr = new google.translate.TranslateElement({
+        pageLanguage: lang,
+        // includedLanguages: 'en,fr,de',
+        layout: 2
+      },
+        'google_translate_element');
+    }, 500);
+
+  }
 }
+}
+
+
+
